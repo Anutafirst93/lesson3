440880af596ab65f9d6ec39584b8a1176d3fc1fe
ANN <Anuta-first@mail.ru>
2015-11-12 21:19:23 +0200

    mes

--- hometasks/L5/src/main/java/Sort.java
+++ hometasks/L5/src/main/java/Sort.java
@@ -1,37 +1,24 @@
 /**
- * Created by Нюта on 12.11.2015.
+ * Created by РќСЋС‚Р° on 12.11.2015.
  */
 public class Sort {
     public static void main(String[] args) {
-        int Arr[];
-        Arr = new int[13];
-        Arr[0] = 3;
-        Arr[1] = 7;
-        Arr[2] = 13;
-        Arr[3] = 21;
-        Arr[4] = 12;
-        Arr[5] = 9;
-        Arr[6] = 4;
-        Arr[7] = 17;
-        Arr[8] = 25;
-        Arr[9] = 31;
-        Arr[10] = 11;
-        Arr[11] = 5;
-        Arr[12] = 1;
-        // реализация Bubble sort (сокр. фрагмета)
-        for (int i = Arr.length-1; i >= 0; i--) {
-            //max элемент в конец фрагмента
-            for (int j = 0; j < i; j++) {
-                //попарное сравнение элементов
+        int [] Arr = {3,7,13,21,12,9,4,17,25,31,11,5,1};
+        // СЂРµР°Р»РёР·Р°С†РёСЏ Bubble sort (СЃРѕРєСЂ. С„СЂР°РіРјРµС‚Р°)
+        for (int i = 0; i < Arr.length-1; i++) {
+            int x = Arr.length-i-1;
+            //max СЌР»РµРјРµРЅС‚ РІ РєРѕРЅРµС† С„СЂР°РіРјРµРЅС‚Р°
+            for (int j = 0; j < x; j++) {
+                //РїРѕРїР°СЂРЅРѕРµ СЃСЂР°РІРЅРµРЅРёРµ СЌР»РµРјРµРЅС‚РѕРІ
                 if (Arr[j] > Arr[j + 1]) {
                     int rev = Arr[j];
                     Arr[j] = Arr[j + 1];
                     Arr[j + 1] = rev;
                 }
-
             }
 
             System.out.print(Arr[i] + " ");
+
         }
     }
 }
