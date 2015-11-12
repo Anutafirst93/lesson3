32022ded7ca1c2bbc779531b9ef7bcb88ee1f40d
ANN <Anuta-first@mail.ru>
2015-11-12 22:51:46 +0200

    mes

--- hometasks/L5/src/main/java/Capac.java
+++ hometasks/L5/src/main/java/Capac.java
@@ -1,16 +1,19 @@
 /**
- * Created by Íşòà on 12.11.2015.
+ * Created by ĞÑÑ‚Ğ° on 12.11.2015.
  */
 public class Capac {
     public static void main(String[] args) {
         int[] Arr = {3, 7, 13, 21, 12, 9, 4, 17, 25, 31, 11, 5, 1};
-        System.out.print(Arr);
         int temp[] = new int[Arr.length + 3];
-
-        for (int i = 0; i <=Arr.length; i++ ) {
-             temp = Arr;
-        }else{ 
-            int j = -1;
-        } System.out.print(temp);
+        for (int i=0; i<temp.length; i++){
+            if ( i < Arr.length) {
+                temp[i] = Arr[i];
+            }else{
+                 temp[i] = -1;
+            }
+        }
+        for (int i=0; i<temp.length; i++){
+        System.out.print(temp[i] + " ");
+        }
     }
 }
